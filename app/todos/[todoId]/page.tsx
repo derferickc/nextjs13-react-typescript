8edf619a833783351a7c08ada3f2df76f7f75cb4
derferickc <frederickchoe@gmail.com>
2023-01-25 02:39:56 -0800

    404 and sub layout created

--- app/todos/[todoId]/page.tsx
+++ app/todos/[todoId]/page.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Todo } from "../../../typings";
 import Todos from "../page";
+import { notFound } from "next/navigation";
+
+export const dynamicParams = true;
 
 type PageProps = {
   params: {
@@ -20,6 +23,8 @@ const fetchTodo = async (todoId: string) => {
 
 async function TodoPage({ params: { todoId } }: PageProps) {
   const todo = await fetchTodo(todoId);
+
+  if (!todo.id) return notFound();
   return (
     <div className="p-10 bg-yellow-200 border-2 m-2 shadow-lg">
       <p>
